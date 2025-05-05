38b4a6c9223e91848ea82ba5f6bb0fd1bc9bf225
RichelleTiongson <richelletiongson@icloud.com>
2025-05-05 15:56:46 -0700

    Rename pages and style default page

--- linko_whatdafour/app/layout.js
+++ linko_whatdafour/app/layout.js
@@ -37,7 +37,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div className = {"wrapper"}>
         {children}
+        </div>
       </body>
     </html>
   );
