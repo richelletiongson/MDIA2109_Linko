823d0bdcc0080d21fe154ae4b772c82797584a41
richelletiongson <richelletiongson@icloud.com>
2025-05-17 08:54:27 -0700

    Delete .history directory

--- .history/MDIA2109_Linko/linko_whatdafour/app/homepage/page_20250508220440.js
+++
@@ -1,7 +0,0 @@
-<Container
-  text="hhh"
-  color="black"
-  size="small"
-  border={true}
-  textStyle="Regular"
-/>
--- .history/MDIA2109_Linko/linko_whatdafour/app/homepage/page_20250508220923.js
+++
@@ -1,8 +0,0 @@
-import Container from "../components/container/container";
-<Container
-  text="hhh"
-  color="black"
-  size="small"
-  border={true}
-  textStyle="Regular"
-/>
