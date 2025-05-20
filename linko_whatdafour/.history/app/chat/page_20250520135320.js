a24b81290b8c00b8d66dada8e9332e266bd958b5
BonnieWan1214 <swan30@my.bcit.ca>
2025-05-20 16:55:31 -0700

    add channel chat page

---
+++ linko_whatdafour/.history/app/chat/page_20250520135320.js
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import { WarningMessage, GetStartedModal } from "../.components/Modal/modal";
+import { Button } from "../.components/buttons";
+
+export default function Page() {
+    const [showWarning, setShowWarning] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <main
+            style={{ padding: "2rem", background: "#222", minHeight: "100vh" }}>
+            <button onClick={() => setShowWarning(!showWarning)}>
+                Toggle Warning
+            </button>
+
+            <button onClick={() => setShowModal(true)}>Show Modal</button>
+
+            {showWarning && (
+                <div style={{ marginTop: "2rem" }}>
+                    <WarningMessage
+                        message='Please write an answer to continue !'
+                        type='error'
+                        size='medium'
+                    />
+                </div>
+            )}
+
+            sdlaksjdalksjda
+            
+
+            {showModal && (
+                <GetStartedModal
+                    isOpen={true}
+                    size='small'
+                    footer={
+                        <Button
+                            type='pink'
+                            size='small_round'
+                            buttonText='>'
+                        />
+                    }>
+                    Cool! Let's move on to the next step
+                    <br />
+                    and get to know you better
+                </GetStartedModal>
+            )}
+        </main>
+    );
+}
---
+++ linko_whatdafour/.history/app/explore/chat/page_20250520151310.js
@@ -0,0 +1,99 @@
+"use client";
+import Image from "next/image";
+import styles from "./chat.module.css";
+import { Container } from "@/app/.components/container/container";
+import NavigationBar from "@/app/.components/Navigation Bar/navigation";
+import React, { useRef, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const messages = [
+  {
+    avatar: "/photos/chat01.png",
+    text: "Hi guys!",
+    self: false,
+  },
+  {
+    avatar: "/photos/chat01.png",
+    text: "Is anyone from Burnaby?",
+    self: false,
+  },
+  {
+    avatar: "/photos/chat01.png",
+    text: "I'm going there alone",
+    self: false,
+  },
+  {
+    avatar: "/photos/chat01.png",
+    text: "Feel free to add me!",
+    self: false,
+  },
+  {
+    avatar: "/photos/chat02.png",
+    text: "Heyyyyyy!",
+    self: false,
+  },
+  {
+    avatar: "/photos/chat02.png",
+    text: "ADDED!",
+    self: false,
+  },
+  {
+    avatar: "/photos/chat03.png",
+    text: "Hellooooo",
+    self: false,
+  },
+  {
+    avatar: "/photos/chat04.png",
+    text: "Can we sell the ticket here?",
+    self: false,
+  },
+  {
+    avatar: "/photos/chat05.png",
+    text: "Noo! You will be banned if you do",
+    self: false,
+  },
+  {
+    avatar: "/photos/chat05.png",
+    text: "I saw it in the rules before joined",
+    self: false,
+  },
+];
+
+export default function ChatPage() {
+  const router = useRouter();
+  const chatEndRef = useRef(null);
+
+  useEffect(() => {
+    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      {/* Header */}
+      <div className={styles.header}>
+        <button className={styles.backBtn} onClick={() => router.back()}>&lt;</button>
+        <span className={styles.headerTitle}>Jennie Kim, BC</span>
+        <button className={styles.menuBtn}>⋮</button>
+      </div>
+      {/* Chat Area */}
+      <div className={styles.chatContainer}>
+        {messages.map((msg, idx) => (
+          <div className={styles.messageRow} key={idx} style={{ justifyContent: msg.self ? "flex-end" : "flex-start" }}>
+            {!msg.self && (
+              <Image src={msg.avatar} alt="avatar" width={38} height={38} style={{ borderRadius: '50%', objectFit: 'cover' }} />
+            )}
+            <Container color={msg.self ? "white" : "white"} size={msg.text.length > 30 ? "chatSizeLong" : msg.text.length > 18 ? "chatSizeMedium" : "chatSizeShort"}>
+              <div className={msg.self ? `${styles.bubble} ${styles.bubbleSelf}` : styles.bubble}>{msg.text}</div>
+            </Container>
+          </div>
+        ))}
+        <div ref={chatEndRef} />
+      </div>
+      {/* Input Area */}
+      <div className={styles.inputArea}>
+        <input className={styles.input} placeholder="Aa" />
+        <button className={styles.sendBtn}>+</button>
+      </div>
+    </div>
+  );
+}
